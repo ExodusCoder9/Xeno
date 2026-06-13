c20fdd1784e3ca5e1224c413cf4359fd21c7bb39
nihal <nihalsadathleo@gmail.com>
2026-06-13 14:54:47 +0530

    First Commit ( No code yet )

--- src/client/java/com/xeno/client/XenoClient.java
+++ src/client/java/com/xeno/client/XenoClient.java
@@ -1,8 +1,11 @@
 package com.xeno.client;
 
+import com.xeno.Initializer;
 import net.fabricmc.api.ClientModInitializer;
+
 public class XenoClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		Initializer.init();
 	}
 }
