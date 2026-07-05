c7a2cdc9be0109934242d4110ea09737f924ba14
exoduscoder9 <nihalsadathleo@gmail.com>
2026-07-05 11:29:38 +0530

    Commit

--- src/client/resources/assets/minecraft/shaders/include/chunksection.glsl
+++ src/client/resources/assets/minecraft/shaders/include/chunksection.glsl
@@ -10,11 +10,14 @@ layout(std140) uniform ChunkSection {
 };
 
 #extension GL_ARB_shader_draw_parameters : enable
+#extension GL_EXT_shader_draw_parameters : enable
 
-// In vertex shader, use gl_DrawIDARB when available
+// In vertex shader, use gl_DrawID when available
 // In fragment shader, use the flat instanceId varying passed from vertex shader
 #ifdef VERTEX_SHADER
-  #ifdef GL_ARB_shader_draw_parameters
+  #if defined(GL_EXT_shader_draw_parameters)
+    #define INSTANCE_ID gl_DrawID
+  #elif defined(GL_ARB_shader_draw_parameters)
     #define INSTANCE_ID gl_DrawIDARB
   #else
     #define INSTANCE_ID gl_InstanceID
