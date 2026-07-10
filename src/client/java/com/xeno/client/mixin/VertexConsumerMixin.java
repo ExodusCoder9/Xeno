1b2267b6040d332fe8092079d340cd6d1d6350d7
exoduscoder9 <nihalsadathleo@gmail.com>
2026-07-11 00:10:37 +0530

    Commit

--- src/client/java/com/xeno/client/mixin/VertexConsumerMixin.java
+++ src/client/java/com/xeno/client/mixin/VertexConsumerMixin.java
@@ -3,7 +3,7 @@ package com.xeno.client.mixin;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import org.spongepowered.asm.mixin.Mixin;
 
-// XENO: Quad interception removed. Dynamic face dropping during static mesh compilation
+//  Quad interception removed. Dynamic face dropping during static mesh compilation
 // was the primary cause of buffer tearing and missing geometry upon camera rotation.
 @Mixin(VertexConsumer.class)
 public interface VertexConsumerMixin {
